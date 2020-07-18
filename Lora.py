3622d0d7bb0e49ae3a6d2e469fda4403d9a6ba5c
“zjy2414” <zjy2414@outlook.com>
2020-07-18 10:12:51 +0800

    .

---
+++ Lora.py
@@ -0,0 +1,9 @@
+import serial
+import time
+
+#Lora信通发送数据
+def Send(message):
+    ser=serial.Serial("/dev/ttyUSB0",9600,timeout=1)     #配置Lora通讯串口
+    ser.write(message)
+    time.sleep(0.5)
+    ser.close()
