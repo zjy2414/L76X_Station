01837520ef43bdaaf59951dcd4376af822f938cf
“zjy2414” <zjy2414@outlook.com>
2020-07-18 13:26:50 +0800

    .

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import L76X
 import time
 import math
 import Lora
+import daemon
 
-try:
-    x=L76X.L76X()
-    x.L76X_Set_Baudrate(9600)
-    x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
-    time.sleep(2)
-    x.L76X_Set_Baudrate(115200)
+def Main():
+    try:
+       x=L76X.L76X()
+       x.L76X_Set_Baudrate(9600)
+       x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+       time.sleep(2)
+       x.L76X_Set_Baudrate(115200)
 
-    x.L76X_Send_Command(x.SET_POS_FIX_400MS);
+       x.L76X_Send_Command(x.SET_POS_FIX_400MS);
 
-    #Set output message
-    x.L76X_Send_Command(x.SET_NMEA_OUTPUT);
+       #Set output message
+       x.L76X_Send_Command(x.SET_NMEA_OUTPUT);
 
-    x.L76X_Exit_BackupMode();
-    while(1):
-        x.L76X_Gat_GNRMC()
-        try:
-            x.Get_GNGGA()
-        except:
-            Lora.Send('error!!')
+       x.L76X_Exit_BackupMode();
+       while(1):
+          x.L76X_Gat_GNRMC()
+          try:
+              x.Get_GNGGA()
+          except:
+              Lora.Send('error!!')
         
         
 
@@ -44,3 +46,6 @@ except:
     #GPIO.cleanup()
     print "\nProgram end"
     exit()
+
+with daemon.DaemonContext():
+    Main()
